e6a588e6700e051069f0abdb93bd985cbbec8a6c
Eric Saucedo <ericalexanders@gmail.com>
2024-03-25 07:57:53 -0300

    Fix: minor fix

--- src/google-generative-ai/google-generative-ai.service.ts
+++ src/google-generative-ai/google-generative-ai.service.ts
@@ -11,8 +11,10 @@ export class GoogleGenerativeAiService {
 
   async generateContent(prompt: string): Promise<any> {
     try {
-      const apiKey = this.configService.get<string>('API_KEY_GOOGLE_AI');
-      const cx = this.configService.get<string>('API_KEY_GOOGLE_CX');
+      // const apiKey = this.configService.get<string>('API_KEY_GOOGLE_AI');
+      // const cx = this.configService.get<string>('API_KEY_GOOGLE_CX');
+      const apiKey = 'AIzaSyACf7KGpkjwep1Jg8mMc6DN-Sr5FZnvkS0';
+      const cx = 'f6ae169c1d2234a19';
       const url = `https://www.googleapis.com/customsearch/v1?q="${prompt}"&cx=${cx}&key=${apiKey}&searchType=image`;
       const response = await this.httpService.get(url).toPromise();
       return response.data.items;
